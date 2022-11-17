19ac15c8057575f30ec6c8775eb2e71d927aa1e0
Blas1ed <117614416+Blas1ed@users.noreply.github.com>
2022-11-16 19:12:37 -0800

    COOP.ps1
    lol

---
+++ COOP.ps1
@@ -0,0 +1,9 @@
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+[System.Windows.MessageBox]::Show('Imagine Being Hacked LOL','Game input','YesNoCancel','Error')
+[System.Windows.MessageBox]::Show('Imagine Being Hacked LOL','Game input','YesNoCancel','Error')
+[System.Windows.MessageBox]::Show('Imagine Being Hacked LOL','Game input','YesNoCancel','Error')
+[System.Windows.MessageBox]::Show('Imagine Being Hacked LOL','Game input','YesNoCancel','Error')
+[System.Windows.MessageBox]::Show('Imagine Being Hacked LOL','Game input','YesNoCancel','Error')
+[System.Windows.MessageBox]::Show('Imagine Being Hacked LOL','Game input','YesNoCancel','Error')
+[System.Windows.MessageBox]::Show('Imagine Being Hacked LOL','Game input','YesNoCancel','Error')
+[System.Windows.MessageBox]::Show('┌∩┐(◣_◢)┌∩┐','Game input','YesNoCancel','Error')
